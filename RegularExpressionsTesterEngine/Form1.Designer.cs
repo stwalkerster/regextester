ebf53e91ca0e35b6ed8fe1bcb786d40b101541cd
Simon Walker <github@helpmebot.org.uk>
2010-08-29 10:42:40 +0000

    add project to subversion

---
+++ RegularExpressionsTesterEngine/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace RegularExpressionsTesterEngine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if( disposing && ( components != null ) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent( )
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager( typeof( Form1 ) );
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer( );
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer( );
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip( );
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer( );
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer( );
+            this.textBox1 = new System.Windows.Forms.TextBox( );
+            this.textBox2 = new System.Windows.Forms.TextBox( );
+            this.textBox3 = new System.Windows.Forms.TextBox( );
+            this.treeView1 = new System.Windows.Forms.TreeView( );
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton( );
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton( );
+            this.toolStripContainer1.ContentPanel.SuspendLayout( );
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout( );
+            this.toolStripContainer1.SuspendLayout( );
+            ( (System.ComponentModel.ISupportInitialize)( this.splitContainer1 ) ).BeginInit( );
+            this.splitContainer1.Panel1.SuspendLayout( );
+            this.splitContainer1.Panel2.SuspendLayout( );
+            this.splitContainer1.SuspendLayout( );
+            this.toolStrip1.SuspendLayout( );
+            ( (System.ComponentModel.ISupportInitialize)( this.splitContainer2 ) ).BeginInit( );
+            this.splitContainer2.Panel1.SuspendLayout( );
+            this.splitContainer2.Panel2.SuspendLayout( );
+            this.splitContainer2.SuspendLayout( );
+            ( (System.ComponentModel.ISupportInitialize)( this.splitContainer3 ) ).BeginInit( );
+            this.splitContainer3.Panel1.SuspendLayout( );
+            this.splitContainer3.Panel2.SuspendLayout( );
+            this.splitContainer3.SuspendLayout( );
+            this.SuspendLayout( );
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add( this.splitContainer1 );
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size( 683, 251 );
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.Location = new System.Drawing.Point( 0, 0 );
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size( 683, 276 );
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add( this.toolStrip1 );
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point( 0, 0 );
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add( this.splitContainer2 );
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add( this.splitContainer3 );
+            this.splitContainer1.Size = new System.Drawing.Size( 683, 251 );
+            this.splitContainer1.SplitterDistance = 128;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Items.AddRange( new System.Windows.Forms.ToolStripItem[ ] {
+            this.toolStripButton1,
+            this.toolStripButton2} );
+            this.toolStrip1.Location = new System.Drawing.Point( 3, 0 );
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size( 165, 25 );
+            this.toolStrip1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point( 0, 0 );
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add( this.textBox1 );
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add( this.textBox2 );
+            this.splitContainer2.Size = new System.Drawing.Size( 683, 128 );
+            this.splitContainer2.SplitterDistance = 349;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point( 0, 0 );
+            this.splitContainer3.Name = "splitContainer3";
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add( this.treeView1 );
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add( this.textBox3 );
+            this.splitContainer3.Size = new System.Drawing.Size( 683, 119 );
+            this.splitContainer3.SplitterDistance = 227;
+            this.splitContainer3.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point( 0, 0 );
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size( 349, 128 );
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.LightCoral;
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point( 0, 0 );
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size( 330, 128 );
+            this.textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Plum;
+            this.textBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox3.Location = new System.Drawing.Point( 0, 0 );
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size( 452, 119 );
+            this.textBox3.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point( 0, 0 );
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size( 227, 119 );
+            this.treeView1.TabIndex = 0;
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Font = new System.Drawing.Font( "Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ( (byte)( 0 ) ) );
+            this.toolStripButton1.Image = ( (System.Drawing.Image)( resources.GetObject( "toolStripButton1.Image" ) ) );
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size( 46, 22 );
+            this.toolStripButton1.Text = "Match";
+            this.toolStripButton1.Click += new System.EventHandler( this.toolStripButton1_Click );
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.CheckOnClick = true;
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ( (System.Drawing.Image)( resources.GetObject( "toolStripButton2.Image" ) ) );
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size( 76, 22 );
+            this.toolStripButton2.Text = "Auto-Match";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size( 683, 276 );
+            this.Controls.Add( this.toolStripContainer1 );
+            this.Name = "Form1";
+            this.Text = "Regular Expressions Tester Engine";
+            this.toolStripContainer1.ContentPanel.ResumeLayout( false );
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout( false );
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout( );
+            this.toolStripContainer1.ResumeLayout( false );
+            this.toolStripContainer1.PerformLayout( );
+            this.splitContainer1.Panel1.ResumeLayout( false );
+            this.splitContainer1.Panel2.ResumeLayout( false );
+            ( (System.ComponentModel.ISupportInitialize)( this.splitContainer1 ) ).EndInit( );
+            this.splitContainer1.ResumeLayout( false );
+            this.toolStrip1.ResumeLayout( false );
+            this.toolStrip1.PerformLayout( );
+            this.splitContainer2.Panel1.ResumeLayout( false );
+            this.splitContainer2.Panel1.PerformLayout( );
+            this.splitContainer2.Panel2.ResumeLayout( false );
+            this.splitContainer2.Panel2.PerformLayout( );
+            ( (System.ComponentModel.ISupportInitialize)( this.splitContainer2 ) ).EndInit( );
+            this.splitContainer2.ResumeLayout( false );
+            this.splitContainer3.Panel1.ResumeLayout( false );
+            this.splitContainer3.Panel2.ResumeLayout( false );
+            this.splitContainer3.Panel2.PerformLayout( );
+            ( (System.ComponentModel.ISupportInitialize)( this.splitContainer3 ) ).EndInit( );
+            this.splitContainer3.ResumeLayout( false );
+            this.ResumeLayout( false );
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+    }
+}
+
---
+++ RegularExpressionsTesterEngine/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace RegularExpressionsTesterEngine
+{
+    public partial class Form1 : Form
+    {
+        public Form1( )
+        {
+            InitializeComponent( );
+        }
+
+        private void toolStripButton1_Click( object sender, EventArgs e )
+        {
+            match();
+        }
+
+        private void match()
+        {
+            Regex r = new Regex(textBox2.Text);
+            Match m = r.Match(textBox1.Text);
+        }
+    }
+}
